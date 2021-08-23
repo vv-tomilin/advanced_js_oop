3a491ce458bebaf743c5c79a8b41d6ad1876631b
vv_tomilin <backatom1@gmail.com>
2021-08-23 16:24:39 +0500

    Less 28 (Наследование классов. Создаем общий прототип слайдера и главные слайдеры)

--- src/js/modules/slider/slider-main.js
+++ src/js/modules/slider/slider-main.js
@@ -1,9 +1,8 @@
-export default class Slider {
+import Slider from './slider';
+
+export default class MainSlider extends Slider {
     constructor(page, btns) {
-        this.page = document.querySelector(page);
-        this.slides = this.page.children;
-        this.btns = document.querySelectorAll(btns);
-        this.slideIndex = 1;
+        super(page, btns);
     }
 
     showSlides(n) {
@@ -67,4 +66,4 @@ export default class Slider {
 
         this.showSlides(this.slideIndex);
     }
-};
+}
