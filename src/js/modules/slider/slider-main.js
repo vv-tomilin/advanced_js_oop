8e50b6b4a657c2b0a6bb79ce5a02203e399ee3f9
vv_tomilin <backatom1@gmail.com>
2021-08-27 16:45:48 +0500

    Less 32 (Многостраничность. Как избегать ошибок в коде)

--- src/js/modules/slider/slider-main.js
+++ src/js/modules/slider/slider-main.js
@@ -45,25 +45,28 @@ export default class MainSlider extends Slider {
     render() {
 
         try {
-            this.hanson = document.querySelector('.hanson');
-        }
-        catch(e) {
-            console.log(e);
-        }
-
-        this.btns.forEach((item) => {
-            item.addEventListener('click', () => {
-                this.plusSlides(1);
+            try {
+                this.hanson = document.querySelector('.hanson');
+            }
+            catch(e) {
+                console.log(e);
+            }
+    
+            this.btns.forEach((item) => {
+                item.addEventListener('click', () => {
+                    this.plusSlides(1);
+                });
+    
+                item.parentNode.previousElementSibling.addEventListener('click', (e) => {
+                    e.preventDefault();
+    
+                    this.slideIndex = 1;
+                    this.showSlides(this.slideIndex);
+                });
             });
-
-            item.parentNode.previousElementSibling.addEventListener('click', (e) => {
-                e.preventDefault();
-
-                this.slideIndex = 1;
-                this.showSlides(this.slideIndex);
-            });
-        });
-
-        this.showSlides(this.slideIndex);
+    
+            this.showSlides(this.slideIndex);
+        }
+        catch(e) {console.log(e);}
     }
 }
